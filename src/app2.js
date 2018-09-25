b3931602fa417bd583623144821f121bf22b73ad
saadkhan6670 <saad.khan6670@gmail.com>
2018-09-26 01:59:28 +0500

    chnages

--- src/app2.js
+++ src/app2.js
@@ -11,7 +11,7 @@ import {
 const MyMapComponent = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCDWcfh74yQKauMTvz8l9A75gURn-zDfw4&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
